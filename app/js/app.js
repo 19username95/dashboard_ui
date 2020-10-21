cc99426faf494b340ed98303f5d049074d14974e
unknown <kazakovayana@outlook.com>
2020-10-21 16:21:25 +0300

    clean

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
+$( document ).ready(function() {
 });
-
-for (let elementNodeListOfElement of document.querySelectorAll('.menu__tab')) {
-
-}
 
 $('.menu__tabs .menu__tab').on('click', function (e) {
   e.preventDefault();
   $(this).siblings().removeClass('menu__tab_active');
   $(this).addClass('menu__tab_active');
-  let target = $(this).attr('data-page');
+  let target = $(this).attr('data-page-id');
   $(target).addClass('content__page_active');
   $('.content > div').not(target).removeClass('content__page_active');
 });
