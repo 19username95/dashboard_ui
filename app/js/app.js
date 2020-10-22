a2e2aab1ff227fd0e3cae5280e90caf2bdff5e78
unknown <kazakovayana@outlook.com>
2020-10-22 21:55:19 +0300

    show tickets from json

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,9 @@ $( document ).ready(function() {
   fetch('/mocks/summary.json')
     .then(res => res.json())
     .then(renderSummary);
+  fetch('/mocks/all-tickets.json')
+    .then(tickets => tickets.json())
+    .then(renderAllTickets);
 });
 
 // menu logic
@@ -23,6 +26,38 @@ const renderSummary = ({ summaryCards }) => {
         <div class="summary__cards--title">${card.name}</div>
         <div class="summary__cards--amount">${card.amount}</div>
       </div>
+    `);
+  });
+}
+
+// ball tickets from json
+const renderAllTickets = ({ tickets }) => {
+  const container = $('.all-tickets__tickets');
+  tickets.map((ticket, i) => {
+    let date = new Date(ticket.deadlineDate);
+
+    container.append(`
+    <tr class="all-tickets__ticket">
+
+        <th class="all-tickets__ticket-details">
+            <div class="all-tickets__ticket-photo"><img alt="" src="${ticket.photoUrl}"/></div>
+            <div class="all-tickets__ticket-title">${ticket.title}</div>
+            <div class="all-tickets__last-update">Updated ${ticket.lastUpdate} ago</div>
+        </th>
+
+         <th class="all-tickets__customer-name">
+            <div class="all-tickets__customer-name">${ticket.customerName} ${ticket.customerSurname}</div>
+            <div class="all-tickets__createdDate">on ${ticket.createdDate}</div>
+         </th>
+         <th class="all-tickets__date-time">
+            <div class="all-tickets__date">${moment(ticket.deadlineDate).format("MMM DD, YYYY")}</div>
+            <div class="all-tickets__date">${moment(ticket.deadlineDate).format('LT')}</div>
+         </th>
+         <th class="all-tickets__priority">${ticket.priority}</th>
+         <th class="all-tickets__more-button">
+            <i class="fas fa-ellipsis-v"></i>
+         </th>
+      </tr>
     `);
   });
 }
